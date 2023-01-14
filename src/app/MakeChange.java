c58c28f14cc2941adcad0c578e6a9208e1fa7c8f
EricHuettel <ericody7@gmail.com>
2023-01-13 19:04:04 -0600

    working starter code

--- src/app/MakeChange.java
+++ src/app/MakeChange.java
@@ -1,10 +1,23 @@
 package app;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner kb = new Scanner(System.in);
+		System.out.print("What is the total of your purchase: ");
+		double purchaseTotal = kb.nextDouble();
+		System.out.print("What is the amount tendered: ");
+		double tenderedTotal = kb.nextDouble();
+		if (tenderedTotal < purchaseTotal)
+			System.out.println("Insufficient Funds");
+		
+		
+		
+		
+		
+		kb.close();
 	}
 
 }
